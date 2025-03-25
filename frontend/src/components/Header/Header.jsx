894c4ba49bb89022aaf9f44047ef3984132be645
Sajjad Ali <sajadalisk958@gmail.com>
2025-03-25 16:28:07 +0500

    navabar finished

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { navbar } from "./Navbar";
+import { FaBarsStaggered } from "react-icons/fa6";
+
+const Header = () => {
+    const [menu, setMenu] = useState(false);
+
+  return (
+    <>
+      {/* computer view */}
+      <div className="lg:flex hidden md:flex item-center justify-between fixed top-0 left-0 w-full lg:px-20 md:px-10 px-3 shadow-sm py-4">
+        <div>
+          <h1 className="font-semibold">
+            JOB<span className="text-purple-500"> HUB</span>
+          </h1>
+        </div>
+
+        <div className="space-x-10">
+          {navbar.map((item, index) => {
+            return (
+              <NavLink
+                to={item.path}
+                key={index}
+                className="text-gray-900 font-semibold"
+                activeClassName="active"
+              >
+                {item.title}
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* mobile view */}
+      <div className="lg:hidden md:hidden fixed top-0 left-0 w-full">
+        <div className="flex items-center justify-between w-full shadow-sm px-3 py-4">
+          <h1 className="font-semibold">
+            JOB<span className="text-purple-500"> HUB</span>
+          </h1>
+        <FaBarsStaggered className="text-purple-500" onClick={() => setMenu(!menu)} />
+        </div>
+
+        <div className={`flex flex-col space-x-10 h-screen w-full bg-white transition-transform duration-300 ease-in-out space-y-5 px-3 mt-3 ${menu ? "translate-x-0 opacity-100" : "-translate-x-full opacity-70"}`}>
+          {navbar.map((item, index) => {
+            return (
+              <NavLink
+                onClick={() => setMenu(false)}
+                to={item.path}
+                key={index}
+                className="text-gray-900 font-semibold"
+                activeClassName="active"
+              >
+                {item.title}
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Header/Navbar.js
@@ -0,0 +1,18 @@
+export const navbar = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "Jobs",
+        path: "/jobs"
+    },
+    {
+        title: "Browse",
+        path: "/browse"
+    },
+    {
+        title: "Login",
+        path: "/login"
+    },
+];
